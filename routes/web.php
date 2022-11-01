d41a046e4933166fe93fdbb22c976ff20bb2ea7a
zainimenglixiao <985184668@qq.com>
2022-11-01 07:55:14 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
