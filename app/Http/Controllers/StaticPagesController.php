97102afee133bcc0d1cede6de5b4e4b572e35992
zainimenglixiao <985184668@qq.com>
2022-11-01 05:37:37 +0000

    模板切分

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home()
+     public function home()
     {
         return view('static_pages/home');
     }
