fecb73a6a7572a8d4402c87794fdb5d913458197
zainimenglixiao <985184668@qq.com>
2022-11-04 08:08:03 +0000

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
-        'introduction'
+        'introduction',
+        'avatar',
     ];
 
     /**
